0ea762170f8afaffa819cc4c1ae153a7dd8b0b00
Billy Kim <billyhkim.dev@gmail.com>
2019-09-15 00:02:15 -0700

    successfully changed json data file

--- playground/1-json.js
+++ playground/1-json.js
@@ -12,4 +12,10 @@ const fs = require('fs');
 const dataBuffer = fs.readFileSync('1-json.json');
 const dataJSON = dataBuffer.toString();
 const data = JSON.parse(dataJSON);
-console.log(data.title);
+
+// change json data
+data.name = 'Billy';
+data.age = 33;
+
+const newData = JSON.stringify(data);
+fs.writeFileSync('1-json.json', newData);
