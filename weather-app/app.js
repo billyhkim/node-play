f03c4fcdba7fb7aa3dbed819cf509672dd3cea09
Billy Kim <billyhkim.dev@gmail.com>
2019-10-20 13:13:58 -0700

    using request to darksky api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,4 @@
+/* Async Example
 console.log('Starting');
 
 setTimeout(() => console.log('2 second timer'), 2000);
@@ -5,3 +6,13 @@ setTimeout(() => console.log('2 second timer'), 2000);
 setTimeout(() => console.log('0 second timer'), 0);
 
 console.log('Stopping');
+*/
+
+const request = require('request');
+const chalk = require('chalk');
+
+const url = 'https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/37.8267,-122.4233';
+
+request({ url, json: true }, (err, res) => {
+  console.log(res.body.currently);
+});
