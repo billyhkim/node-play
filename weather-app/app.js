e7d178a574a2f82126d844fbecc44925ed851b93
Billy Kim <billyhkim.dev@gmail.com>
2019-10-20 13:25:08 -0700

    successfully requesting current temp and precipProb

--- weather-app/app.js
+++ weather-app/app.js
@@ -11,8 +11,9 @@ console.log('Stopping');
 const request = require('request');
 const chalk = require('chalk');
 
-const url = 'https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/33.8366,-117.9143';
 
 request({ url, json: true }, (err, res) => {
-  console.log(res.body.currently);
+  // console.log(res.body.currently);
+  console.log(`It is currently ${chalk.bold.green(res.body.currently.temperature)} degrees. There is a ${chalk.bold.blue(res.body.currently.precipProbability)}% chance of rain.`);
 });
