16a20c7bc80abf36823cea6cac8a668d332c39e0
Billy Kim <billyhkim.dev@gmail.com>
2019-10-21 02:07:27 -0700

    separating each api call

--- weather-app/app.js
+++ weather-app/app.js
@@ -10,33 +10,38 @@ console.log('Stopping');
 
 const request = require('request');
 const chalk = require('chalk');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const darkskyUrl = 'https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/33.8366,-117.9143?lang=ko&units=us';
+// const darkskyUrl = 'https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/33.8366,-117.9143?lang=ko&units=us';
 
-request({ url: darkskyUrl, json: true }, (err, res) => {
-  if (err) {
-    console.error(`Unable to connect to weather service.`);
-  } else if (res.body.error) {
-    console.error(`Unable to find location.`)
-  } else {
-    console.log(`${chalk.bold.magenta(res.body.daily.data[0].summary)}`);
-    console.log(`It is currently ${chalk.bold.green(res.body.currently.temperature)} degrees. There is a ${chalk.bold.blue(res.body.currently.precipProbability)}% chance of rain.`);
-  }
+// request({ url: darkskyUrl, json: true }, (err, res) => {
+//   if (err) {
+//     console.error(`Unable to connect to weather service.`);
+//   } else if (res.body.error) {
+//     console.error(`Unable to find location.`)
+//   } else {
+//     console.log(`${chalk.bold.magenta(res.body.daily.data[0].summary)}`);
+//     console.log(`It is currently ${chalk.bold.green(res.body.currently.temperature)} degrees. There is a ${chalk.bold.blue(res.body.currently.precipProbability)}% chance of rain.`);
+//   }
+// });
+
+geocode('Irvine California', (err, data) => {
+  console.log('Error: ', err);
+  console.log('Data: ', data);
 });
 
-// Geocoding
-// Goal: Address -> Lat/Long -> Weather
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
 
-const mapboxGeocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmlsbHloa2ltIiwiYSI6ImNrMXpnaWZ2ejB1eGYzaG80cTM0N3N6amoifQ.Jmy5hJVkidza14aFCloW6A&limit=1';
-
-request({ url: mapboxGeocodeUrl, json: true }, (err, res) => {
-  if (err) {
-    console.error(`Unable to connect to geocoding service.`);
-  } else if (res.body.features.length === 0) {
-    console.error(`Not a valid location.`)
-  } else {
-    const longitude = res.body.features[0].center[0];
-    const latitude = res.body.features[0].center[1];
-    console.log(chalk.yellow.bold(`latitude: ${latitude}, longitude: ${longitude}`));
-  }
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error: ', error)
+  console.log('Data: ', data)
 })
