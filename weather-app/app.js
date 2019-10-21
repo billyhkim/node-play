e367e048a48c01c311822afb48eadbb5ec872cf3
Billy Kim <billyhkim.dev@gmail.com>
2019-10-20 22:00:20 -0700

    added geocode url

--- weather-app/app.js
+++ weather-app/app.js
@@ -11,10 +11,21 @@ console.log('Stopping');
 const request = require('request');
 const chalk = require('chalk');
 
-const url = 'https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/33.8366,-117.9143?lang=ko&units=us';
+const darkskyUrl = 'https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/33.8366,-117.9143?lang=ko&units=us';
 
-request({ url, json: true }, (err, res) => {
+request({ url: darkskyUrl, json: true }, (err, res) => {
   // console.log(res.body.currently);
   console.log(`${chalk.bold.magenta(res.body.daily.data[0].summary)}`);
   console.log(`It is currently ${chalk.bold.green(res.body.currently.temperature)} degrees. There is a ${chalk.bold.blue(res.body.currently.precipProbability)}% chance of rain.`);
 });
+
+// Geocoding
+// Goal: Address -> Lat/Long -> Weather
+
+const mapboxGeocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmlsbHloa2ltIiwiYSI6ImNrMXpnaWZ2ejB1eGYzaG80cTM0N3N6amoifQ.Jmy5hJVkidza14aFCloW6A&limit=1';
+
+request({ url: mapboxGeocodeUrl, json: true }, (err, res) => {
+  const longitude = res.body.features[0].center[0];
+  const latitude = res.body.features[0].center[1];
+  console.log(chalk.yellow.bold(`latitude: ${latitude}, longitude: ${longitude}`));
+})
