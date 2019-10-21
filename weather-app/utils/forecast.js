968005ceb2ffdcc78a2742f7e16e3cd4049f5d52
Billy Kim <billyhkim.dev@gmail.com>
2019-10-21 02:20:32 -0700

    created new utils folder with separate api call fxns

---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const forecast = (lat, long, callback) => {
+  const url = `https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/${long},${lat}`;
+
+  request({ url, json: true }, (err, res) => {
+    if (err) {
+      callback(`Unable to connect to weather service.`);
+    } else if (res.body.error) {
+      callback(`Unable to find location.`);
+    } else {
+      callback(undefined, {
+        summary: res.body.daily.data[0].summary,
+        temperature: res.body.currently.temperature,
+        precipitation: res.body.currently.precipProbability
+      });
+    }
+  })
+};
+
+module.exports = forecast;
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const geocode = (address, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiYmlsbHloa2ltIiwiYSI6ImNrMXpnaWZ2ejB1eGYzaG80cTM0N3N6amoifQ.Jmy5hJVkidza14aFCloW6A&limit=1`;
+
+  request({ url, json: true }, (err, res) => {
+    if (err) {
+      callback(`Unable to connect to geocoding service.`);
+    } else if (res.body.features.length === 0) {
+      callback(`Not a valid location.`);
+    } else {
+      callback(undefined, {
+        longitude: res.body.features[0].center[0],
+        latitude: res.body.features[0].center[1],
+        location: res.body.features[0].place_name
+      });
+    }
+  })
+};
+
+module.exports = geocode;
