cc939b739cff061cea49a15dfad8e11cbcc7f2db
Billy Kim <billyhkim.dev@gmail.com>
2019-10-21 11:48:17 -0700

    refactoring forecast.js

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,16 +3,16 @@ const request = require('request');
 const forecast = (lat, long, callback) => {
   const url = `https://api.darksky.net/forecast/081adf3326f39a684339e12e3c742b8d/${lat},${long}`;
 
-  request({ url, json: true }, (err, res) => {
+  request({ url, json: true }, (err, { body }) => {
     if (err) {
       callback(`Unable to connect to weather service.`);
-    } else if (res.body.error) {
+    } else if (body.error) {
       callback(`Unable to find location.`);
     } else {
       callback(undefined, {
-        summary: res.body.daily.data[0].summary,
-        temperature: res.body.currently.temperature,
-        precipitation: res.body.currently.precipProbability
+        summary: body.daily.data[0].summary,
+        temperature: body.currently.temperature,
+        precipitation: body.currently.precipProbability
       });
     }
   })
